7422a3acda8d35381fd0ae7607aa4b04b14aa290
Andrea Tantimonaco <learning@andreatantimonaco.me>
2021-03-27 20:52:55 +0100

    Explaining the router resolve returning structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,12 @@ const routes: Routes = [
   {
     path: 'user', component: UserComponent,
     children: [
+      // Take a look here:
+      // the { user: UserGuard } is telling you what you'll find inside the x argument
+      // in the UserEditorComponent -> resolver subscription... the User object will
+      // be placed into an object with the user key, like this
+      //
+      // { user: User }
       { path: ':name', component: UserEditorComponent, resolve: { user: UserGuard } },
     ],
   }
